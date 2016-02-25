6b26971d48a539e5b7e3b788959857d79afdafbc
AlexPiermatte <alex.piermatteo@mail.polimi.it>
2016-02-25 14:55:55 +0100

    first version

--- src/main/java/org/apache/kafka/connect/binary/BinarySourceTask.java
+++ src/main/java/org/apache/kafka/connect/binary/BinarySourceTask.java
@@ -1,6 +1,9 @@
 package org.apache.kafka.connect.binary;
 
 import org.apache.kafka.common.errors.InterruptException;
+import org.apache.kafka.connect.data.Schema;
+import org.apache.kafka.connect.data.SchemaBuilder;
+import org.apache.kafka.connect.data.Struct;
 import org.apache.kafka.connect.errors.ConnectException;
 import org.apache.kafka.connect.source.SourceRecord;
 import org.apache.kafka.connect.source.SourceTask;
@@ -8,6 +11,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
 
 public class BinarySourceTask extends SourceTask {
@@ -16,6 +23,9 @@ public class BinarySourceTask extends SourceTask {
     private String tmp_path;
 
     private TimerTask task;
+    private static Schema schema = null;
+    private String schemaName;
+    private String topic;
     private Map<String, Object> offsets = new HashMap<>(0);
 
     @Override
@@ -37,6 +47,16 @@ public class BinarySourceTask extends SourceTask {
             }
         };
 
+        schemaName = "filebinaryschema"; //map.get(SocketSourceConnector.SCHEMA_NAME);
+        topic = "filebinary"; //map.get(SocketSourceConnector.TOPIC);
+
+        log.trace("Creating schema");
+        schema = SchemaBuilder
+                .struct()
+                .name(schemaName)
+                .field("name", Schema.OPTIONAL_STRING_SCHEMA)
+                .field("binary", Schema.OPTIONAL_BYTES_SCHEMA)
+                .build();
         Timer timer = new Timer();
         timer.schedule( task , new Date(), 1000 );
         //loadOffsets("connectorname", "partitionname");
@@ -44,8 +64,31 @@ public class BinarySourceTask extends SourceTask {
 
     @Override
     public List<SourceRecord> poll() throws InterruptException {
-        //consue here the pool
+
         List<SourceRecord> records = new ArrayList<>();
+        //consume here the pool
+        while (!((DirWatcher) task).getQueueFiles().isEmpty()) {
+            File file = ((DirWatcher) task).getQueueFiles().poll();
+            byte[] data = null;
+            try {
+                //transform file to byte[]
+                Path path = Paths.get(file.getPath());
+                data = Files.readAllBytes(path);
+                log.error(String.valueOf(data.length));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            // creates the structured message
+            Struct messageStruct = new Struct(schema);
+            messageStruct.put("name", file.getName());
+            messageStruct.put("binary", data);
+            // creates the record
+            // no need to save offsets
+            SourceRecord record = new SourceRecord(Collections.singletonMap("file_binary", 0), Collections.singletonMap("0", 0), topic, messageStruct.schema(), messageStruct);
+            records.add(record);
+        }
+
         return records;
     }
 
