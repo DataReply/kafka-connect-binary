342e1401c59786e24ed67ac4497bbdbf34364c66
AlexPiermatte <alex.piermatteo@mail.polimi.it>
2016-03-10 12:32:30 +0100

    refactor test

--- src/main/java/org/apache/kafka/connect/binary/test/BinarySourceTaskTest.java
+++ src/main/java/org/apache/kafka/connect/binary/test/BinarySourceTaskTest.java
@@ -1,8 +1,8 @@
-package org.apache.kafka.connect.binary;
+package org.apache.kafka.connect.binary.test;
 
 
-import org.apache.kafka.connect.data.Field;
-import org.apache.kafka.connect.data.Schema;
+import org.apache.kafka.connect.binary.BinarySourceTask;
+import org.apache.kafka.connect.binary.utils.SchemaUtils;
 import org.apache.kafka.connect.data.Struct;
 import org.apache.kafka.connect.source.SourceRecord;
 import org.apache.kafka.connect.source.SourceTaskContext;
@@ -22,13 +22,14 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * Created by alex on 09.03.16.
+ * Created by Alex Piermatteo on 09.03.16.
  */
 public class BinarySourceTaskTest {
 
     private BinarySourceTask task;
     private SourceTaskContext context;
     private Map<String, String> sourceProperties;
+    private final String TEST_PATH = "src/main/java/org/apache/kafka/connect/binary/test/";
 
     @Before
     public void setup() {
@@ -40,14 +41,14 @@ public class BinarySourceTaskTest {
         sourceProperties.put("topic", "binarytopic");
         sourceProperties.put("schema.name", "binaryschematest");
         sourceProperties.put("use.java.dirwatcher", "false");
-        sourceProperties.put("filename.path", "test-source.png");
+        sourceProperties.put("filename.path", TEST_PATH + "test-source.png");
     }
 
     @Test
     public void test() throws Exception {
         replay();
 
-        File file = new File("test-cmp.png");
+        File file = new File(TEST_PATH + "test-cmp.png");
         byte[] data = null;
         try {
             //transform file to byte[]
@@ -59,11 +60,11 @@ public class BinarySourceTaskTest {
 
         task.start(sourceProperties);
         List<SourceRecord> records = task.poll();
-        Map<String, Object> map = toJsonMap((Struct) records.get(0).value());
+        Map<String, Object> map = SchemaUtils.toJsonMap((Struct) records.get(0).value());
 
         byte[] bytes = (byte[]) map.get("binary");
 
-        FileOutputStream stream = new FileOutputStream("1_" + (String) map.get("name"));
+        FileOutputStream stream = new FileOutputStream(TEST_PATH + "1_" + (String) map.get("name"));
         try {
             stream.write(bytes);
         } finally {
@@ -76,40 +77,6 @@ public class BinarySourceTaskTest {
 
     private void replay() {
         PowerMock.replayAll();
-    }
-
-
-    private static Map<String, Object> toJsonMap(Struct struct) {
-        Map<String, Object> jsonMap = new HashMap<String, Object>(0);
-        List<Field> fields = struct.schema().fields();
-        for (Field field : fields) {
-            String fieldName = field.name();
-            Schema.Type fieldType = field.schema().type();
-            switch (fieldType) {
-                case STRING:
-                    jsonMap.put(fieldName, struct.getString(fieldName));
-                    break;
-                case INT32:
-                    jsonMap.put(fieldName, struct.getInt32(fieldName));
-                    break;
-                case INT16:
-                    jsonMap.put(fieldName, struct.getInt16(fieldName));
-                    break;
-                case INT64:
-                    jsonMap.put(fieldName, struct.getInt64(fieldName));
-                    break;
-                case FLOAT32:
-                    jsonMap.put(fieldName, struct.getFloat32(fieldName));
-                    break;
-                case BYTES:
-                    jsonMap.put(fieldName, struct.getBytes(fieldName));
-                    break;
-                case STRUCT:
-                    jsonMap.put(fieldName, toJsonMap(struct.getStruct(fieldName)));
-                    break;
-            }
-        }
-        return jsonMap;
     }
 
 }
